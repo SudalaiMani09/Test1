18bf5749eabd2a11e4989c97580d683f9b5dbe2b
Sudalaimani09 <msudalaimani92@gmail.com>
2022-03-06 10:37:05 +0530

    RET123

---
+++ CitiBank.java
@@ -0,0 +1,11 @@
+package org.base;
+
+import org.acc.AxisBank;
+
+public class CitiBank extends AxisBank{
+
+	public static void main(String[] args) {
+		CitiBank a= new CitiBank();
+		a.savings();	}
+
+}
